032efebbdc08494a52acf6b5bc082f96734067e7
Cody Snell <codyjsnell@yahoo.com>
2021-05-13 00:04:47 -0500

    adds imports and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Login from './components/Login'
+import Home from './components/Home'
+import Header from './components/Header'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
