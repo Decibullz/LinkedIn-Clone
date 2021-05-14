42031e6f3f1b07278094bec1a95f0d42dd3ccc57
Cody Snell <codyjsnell@yahoo.com>
2021-05-14 00:31:23 -0500

    styles item component

--- src/components/Leftside.js
+++ src/components/Leftside.js
@@ -26,6 +26,7 @@ const Leftside = (props) => {
         <Item>
           <span>
             <img src="/images/item-icon.svg" alt="" />
+            My items
           </span>
         </Item>
       </ArtCard>
@@ -109,9 +110,45 @@ const Widget = styled.div`
     &:hover {
       background-color: rgba(0, 0, 0, 0.08);
     }
+    div {
+      display: flex;
+      flex-direction: column;
+      text-align: left;
+      span {
+        font-size: 12px;
+        line-height: 1.333;
+        &:first-child {
+          color: rgba(0, 0, 0, 0.6);
+        }
+        &:nth-child(2) {
+          color: rgba(0, 0, 0, 1);
+        }
+      }
+    }
+  }
+
+  svg {
+    color: rgba(0, 0, 0, 1);
   }
 `
 
-const Item = styled.div``
+const Item = styled.a`
+  border-color: rgba(0, 0, 0, 0.8);
+  text-align: left;
+  padding: 12px;
+  font-size: 12px;
+  display: block;
+  span {
+    display: flex;
+    align-items: center;
+    color: rgba(0, 0, 0, 1);
+    svg {
+      color: rgba(0, 0, 0, 0.6);
+    }
+  }
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+  }
+`
 
 export default Leftside
