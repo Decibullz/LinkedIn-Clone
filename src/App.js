4ca8f23cf76cae5c8fff3c4345bca24bc1e87876
Cody Snell <codyjsnell@yahoo.com>
2021-05-14 23:03:01 -0500

    adds get user functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
+import { useEffect } from 'react'
 import Login from './components/Login'
 import Home from './components/Home'
 import Header from './components/Header'
+import { getUserAuth } from './redux/actions'
+import { connect } from 'react-redux'
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    props.getUserAuth()
+  }, [])
   return (
     <div className="App">
       <Router>
@@ -23,4 +28,12 @@ function App() {
   )
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return {}
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  getUserAuth: () => dispatch(getUserAuth()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
